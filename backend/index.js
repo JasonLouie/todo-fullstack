e3170e0e768c7614da2e0393b97912e929057aa6
Jason Louie <jason.louie29@myhunter.cuny.edu>
2025-11-14 09:54:14 -0500

    Connected frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 const port = 8080;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.json({message: "Hello World! (From server)"});
