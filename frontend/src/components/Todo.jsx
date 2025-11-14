17aaec61c5b6ed34125ae383000e5a72069ea2aa
Jason Louie <jason.louie29@myhunter.cuny.edu>
2025-11-14 14:34:39 -0500

    Fixed error with modifying the text of the todo item. Properly disable the save button if no changes were made.

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from "react";
 
 export default function Todo({ _id, text, completed, setTodos }) {
     const [edit, setEdit] = useState(false);
-    const inputRef = useRef(null);
+    const [todoText, setTodoText] = useState(text);
 
     async function handleDelete(id) {
         try {
@@ -29,21 +29,19 @@ export default function Todo({ _id, text, completed, setTodos }) {
     }
 
     async function handleModifyText(id) {
-        if (text !== inputRef.current.value) {
-            try {
-                await fetch(`http://localhost:8080/todos/${id}`, {
-                    method: "PATCH",
-                    body: JSON.stringify({ text: inputRef.current.value }),
-                    headers: {
-                        "Content-Type": "application/json"
-                    }
-                });
-                setTodos(prev => prev.map(p => p._id === id ? {...p, text: inputRef.current.value} : p));
-            } catch (err) {
-                console.log(err);
-            }
-            setEdit(!edit);
+        try {
+            await fetch(`http://localhost:8080/todos/${id}`, {
+                method: "PATCH",
+                body: JSON.stringify({ text: todoText }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            setTodos(prev => prev.map(p => p._id === id ? {...p, text: todoText} : p));
+        } catch (err) {
+            console.log(err);
         }
+        setEdit(!edit);
     }
 
     return (
@@ -51,8 +49,8 @@ export default function Todo({ _id, text, completed, setTodos }) {
             <input type="checkbox" checked={completed} onChange={(e) => toggleComplete(_id, e.target.checked)} />
             {edit ?
                 <>
-                    <input type="text" defaultValue={text} ref={inputRef} />
-                    <button onClick={() => handleModifyText(_id)}>Save</button>
+                    <input type="text" value={todoText} onChange={(e) => setTodoText(e.target.value)} />
+                    <button onClick={() => handleModifyText(_id)} disabled={todoText === text}>Save</button>
                 </> : 
                 <>
                     <span>{text}</span>
